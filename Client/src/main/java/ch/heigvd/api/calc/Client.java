8d5a4b9143a332538130f7962fe984eea579d351
IEscher <54139119+IEscher@users.noreply.github.com>
2022-11-07 16:18:44 +0100

    Client NOT TESTED

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -1,7 +1,8 @@
 package ch.heigvd.api.calc;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,6 +12,8 @@ import java.util.logging.Logger;
 public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
+
+    private static final int port = 3101;
 
     /**
      * Main function to run client
@@ -22,8 +25,9 @@ public class Client {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
         BufferedReader stdin = null;
+        PrintWriter stdout = null;
 
-        /* TODO: Implement the client here, according to your specification
+                /* TODO: Implement the client here, according to your specification
          *   The client has to do the following:
          *   - connect to the server
          *   - initialize the dialog with the server according to your specification
@@ -35,5 +39,45 @@ public class Client {
 
         stdin = new BufferedReader(new InputStreamReader(System.in));
 
+        // Establish connection with the server and set stdout
+        Socket serverSocket;
+        try {
+            serverSocket = new Socket((String) null, port); // Not sure about that
+            stdout = new PrintWriter(serverSocket.getOutputStream());
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            return;
+        }
+
+        // Exchange with the server
+        String line;
+        boolean shouldRun = true;
+        try {
+            LOG.info("Reading until client sends BYE or closes the connection...");
+            while ((shouldRun) && (line = stdin.readLine()) != null) {
+
+                // TODO implement base mechanism here
+
+            }
+
+            LOG.info("Cleaning up resources...");
+            serverSocket.close();
+            stdin.close();
+            stdout.close();
+
+        } catch (IOException ex) { // Close everything properly
+            try {
+                stdin.close();
+            } catch (IOException ex1) {
+                LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
+            }
+            stdout.close();
+            try {
+                serverSocket.close();
+            } catch (IOException ex1) {
+                LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
+            }
+            LOG.log(Level.SEVERE, ex.getMessage(), ex);
+        }
     }
 }
--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/Client.java
+++
@@ -1,5 +0,0 @@
-package ch.heigvd.api.calc;
-
-public class Client {
-    
-}
